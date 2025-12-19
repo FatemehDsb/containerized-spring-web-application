fa0cef2595949ab515b014298577e757a96862a0
Fatemeh Dastgheib <fatemeh.dastgheib@gmail.com>
2025-12-19 10:22:26 +0100

    first commit

---
+++ src/main/java/com/example/containerized_web_application/ContainerizedWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.containerized_web_application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContainerizedWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContainerizedWebApplication.class, args);
+	}
+
+}
